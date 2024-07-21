1c39455b0f29501efbb00b936f61e51746bfefff
o_o <tiamo624@qq.com>
2024-07-22 06:08:43 +0800

    fix error

--- generate_pdf.mjs
+++ generate_pdf.mjs
@@ -1,55 +1,43 @@
 import { readFileSync } from 'fs';
-import { launch } from 'puppeteer';
+import { launch } from 'puppeteer-core'; 
 
-import cv from './cv.json' assert { type: 'json' };
+const cv = JSON.parse(readFileSync(new URL('./resume.json', import.meta.url)));
 
-// import { chromium } from 'playwright-chromium';
-
-const destDir = 'public'  // or 'dist' if we want to generate pdf in GitHub Actions
+const destDir = 'public'; 
 
 async function generatePDF(html, outputPath) {
-  const browser = await launch();
-  const page = await browser.newPage();
-  await page.setContent(html);
-  const pdf = await page.pdf({
-    format: 'A4',
-    displayHeaderFooter: false,
-    preferCSSPageSize: true,
-    path: outputPath,
-  });
-  await browser.close();
-  console.log('generatePDF completed')
-  return pdf;
+  try {
+    const browser = await launch({
+      executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe', 
+      timeout: 0 
+    });
+    const page = await browser.newPage();
+    await page.setContent(html, {timeout: 0}); 
+    const pdf = await page.pdf({
+      format: 'A4',
+      displayHeaderFooter: false,
+      preferCSSPageSize: true,
+      path: outputPath,
+    });
+    await browser.close();
+    console.log('generatePDF completed');
+    return pdf;
+  } catch (error) {
+    console.error('Failed to generate PDF:', error);
+  }
 }
-
-async function generatePDFPlaywright(html, outputPath) {
-  const browser = await chromium.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  await page.setContent(html)
-  await page.pdf({
-    format: 'A4',
-    preferCSSPageSize: true,
-    path: outputPath,
-  });
-  await browser.close();
-}
-
-// read file dist/index.html
 const html = readFileSync('dist/index.html', 'utf8');
 
-// match title
 const titleRegex = /<title>(.*)<\/title>/;
 const title = html.match(titleRegex)[1];
 
-// match semver in title
-const semverRegex = /v?\d+\.\d+\.\d+/;
-const semver = title.match(semverRegex)[0];
-if (semver !== cv.meta.version) {
-  console.warn(`Version mismatch between title (${semver}) and cv.json (${cv.meta.version})`);
-  process.exit(1)
+console.log("call generate pdf");
+try {
+  generatePDF(html, `${destDir}/resume.pdf`).then(() => {
+    console.log('PDF generation succeeded');
+  }).catch((error) => {
+    console.error('PDF generation failed:', error);
+  });
+} catch (error) {
+  console.error('Unexpected error:', error);
 }
-
-console.log("call generate pdf")
-await generatePDF(html, `${destDir}/${title}.pdf`)
-console.log("call end")
